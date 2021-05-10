10b5c877000897a0a1c6d7b04e548c9dd8f3276c
nacerillo <nicholascerillo@gmail.com>
2021-05-10 15:49:07 -0700

    adding basic server setup

---
+++ index-sockets.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const PORT = process.env.PORT || 3000;
+
+const io = require("socket.io")(PORT);
+
+io.on("connection", (socket) => {
+  socket.on("hello", (payload) => {
+    console.log(payload, "said hi");
+    socket.emit("welcome", `Welcome, ${payload}`);
+  });
+});
