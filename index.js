07a622fb3bb356952ad7e9a81c608d1091b08ee9
nacerillo <nicholascerillo@gmail.com>
2021-05-14 17:19:41 -0700

    console deployment should say Welcome Nick

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome, 401d41");
+  res.status(200).send("Welcome, Nick");
 });
 
 app.listen(PORT, () => console.log("Up on", PORT));
